032c7a3de45358e533d2c458d873f6f5a13261ad
Yukihiro Okada <yuokada@users.noreply.github.com>
2024-09-21 14:24:13 +0900

    Initial

---
+++ src/test/java/io/yuokada/quarkus/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package io.yuokada.quarkus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
